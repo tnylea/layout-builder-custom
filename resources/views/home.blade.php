08ee289c49364a07cacd2d9d24512dfa68838a31
Tony Lea <tnylea@gmail.com>
2025-12-19 16:40:33 -0500

    Adding latest updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,20 +11,17 @@
                     type: 'row',
                     rowWidthMode: 'boxed',      // 'full' | 'boxed'
                     maxWidth: '7xl',            // Preset key (e.g., '7xl') or null
-                },
-                {
-                    id: this.generateId(),
-                    type: 'row',
-                    rowWidthMode: 'boxed',
-                    maxWidth: '7xl',
-                },
-                {
-                    id: this.generateId(),
-                    type: 'row',
-                    rowWidthMode: 'boxed',
-                    maxWidth: '7xl',
+                    rows: [{ id: this.generateId() }, { id: this.generateId() }]
                 }
             ];
+        },
+        addRow(layoutIndex) {
+            this.layout[layoutIndex].rows.push({ id: this.generateId() });
+            this.saveHistory();
+        },
+        deleteRow(layoutIndex) {
+            this.layout[layoutIndex].rows.pop();
+            this.saveHistory();
         },
         saveHistory() {
             // Trim future history
@@ -49,16 +46,27 @@
     "
     class="p-5 gap-5 grid">
         <template x-for="(row, rowIndex) in layout" :key="row.id">
-            <div class="relative">
-                <div class="grid grid-rows-2">
-                    <div  class="h-24 hover:ring-2 hover:ring-blue-600 row-span-1 hover:ring-inset relative w-full grid grid-cols-12 border-x border-y border-blue-600 divide-y-4 divide-y-reverse divide-gray-200 bg-gray-200">
-                        row
-                    </div>
-                    <div  class="h-24 hover:ring-2 hover:ring-blue-600 row-span-1 hover:ring-inset relative w-full grid grid-cols-12 border-x border-y border-blue-600 divide-y-4 divide-y-reverse divide-gray-200 bg-gray-200">
-                        row
-                    </div>
+            <div class="relative border border-stone-200 p-5 rounded-xl hover:ring-2 hover:ring-blue-600">
+                <div class="grid border-t border-stone-300 " :style="'grid-template-rows: repeat(' + row.rows.length + ', minmax(0, 1fr))'">
+                    <template x-for="(innerRow, innerRowIndex) in row.rows" :key="innerRow.id">
+                        <div class="h-24 relative w-full grid grid-cols-12 border border-t-0 border-stone-300 bg-gray-100">
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="col-span-1 h-full"></div>
+                        </div>
+                    </template>
                 </div>
-                <button class="rounded-full w-6 h-6 flex items-center justify-center bg-blue-600 -translate-x-1/2 translate-y-1/2 text-white absolute bottom-0 left-0">+</button>
+                <button @click="deleteRow(rowIndex)" x-show="row.rows.length > 1" class="rounded-full w-6 h-6 flex items-center justify-center bg-red-600 -translate-x-1/2 -translate-y-1/2 text-white absolute top-0 left-0">-</button>
+                <button @click="addRow(rowIndex)" class="rounded-full w-6 h-6 flex items-center justify-center bg-blue-600 -translate-x-1/2 translate-y-1/2 text-white absolute bottom-0 left-0">+</button>
             </div>
         </template>
     </section>
