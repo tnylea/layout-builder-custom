c9b947dd8c12f080a77e2cb5b58158ef8aa7d0fa
Tony Lea <tnylea@gmail.com>
2025-12-20 07:34:39 -0500

    latest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,22 +46,22 @@
     "
     class="p-5 gap-5 grid">
         <template x-for="(row, rowIndex) in layout" :key="row.id">
-            <div class="relative border border-stone-200 p-5 rounded-xl hover:ring-2 hover:ring-blue-600">
+            <div class="relative border max-w-7xl mx-auto w-full border-stone-200 p-5 rounded-xl hover:ring-2 hover:ring-blue-600">
                 <div class="grid border-t border-stone-300 " :style="'grid-template-rows: repeat(' + row.rows.length + ', minmax(0, 1fr))'">
                     <template x-for="(innerRow, innerRowIndex) in row.rows" :key="innerRow.id">
-                        <div class="h-24 relative w-full grid grid-cols-12 border border-t-0 border-stone-300 bg-gray-100">
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 border-r border-stone-300 h-full"></div>
-                            <div class="col-span-1 h-full"></div>
+                        <div class="h-auto relative w-full grid grid-cols-12 border border-t-0 border-stone-300 bg-gray-100">
+                            <div class="aspect-square w-full col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 border-r border-stone-300 h-full"></div>
+                            <div class="aspect-square col-span-1 h-full"></div>
                         </div>
                     </template>
                 </div>
